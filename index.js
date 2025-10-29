d343540034b2a7a3f2a0fd0e04774251a30abd62
harrist <harrist@uw.edu>
2025-10-28 17:10:23 -0700

    Download chart

---
+++ index.js
@@ -0,0 +1,50 @@
+/*
+ * Name: Harry Cheng
+ * Date: 10/28/2025
+ * Section: CSE 154 AA
+ */
+
+"use strict"
+(function () {
+  window.addEventListener("load", init);
+
+  function init() {
+
+  }
+
+  /**
+   * Returns the element that has the ID attribute with the specified value.
+   * @param {string} id - element ID.
+   * @returns {object} - DOM object associated with id.
+   */
+  function id(id) {
+    return document.getElementById(id);
+  }
+
+  /**
+   * Returns first element matching selector.
+   * @param {string} selector - CSS query selector.
+   * @returns {object} - DOM object associated selector.
+   */
+  function qs(selector) {
+    return document.querySelector(selector);
+  }
+
+  /**
+   * Returns the array of elements that match the given CSS selector.
+   * @param {string} query - CSS query selector
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+  function qsa(query) {
+    return document.querySelectorAll(query);
+  }
+
+  /**
+   * Returns a element with the given tagname.
+   * @param {string} tagname - HTML element tagname
+   * @returns {HTMLElement} a HTML element that hasn't bind with DOM yet.
+   */
+  function gen(tagname) {
+    return document.createElement(tagname);
+  }
+})();
