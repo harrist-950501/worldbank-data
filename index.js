e2af768294325e71116e8e78626af88a6e4bc7f5
harrist <harrist@uw.edu>
2025-10-31 13:34:41 -0700

    JS linter pass test

--- index.js
+++ index.js
@@ -13,18 +13,22 @@
  */
 
 "use strict";
-(function () {
+(function (){
   const BASE_URL = "https://api.worldbank.org/v2";
-  //Date for world bank data search
+
+  // Date for world bank data search
   const START_YEAR = 2004;
   const END_YEAR = 2023;
-  //Indicator code for world bank data search
+
+  // Indicator code for world bank data search
   const POPULATION_INDICATOR = "SP.POP.TOTL";
   const GDP_INDICATOR = "NY.GDP.MKTP.CD";
-  //The two chart
+
+  // The two chart
   let popChart;
   let gdpChart;
-  //Colors for line, maximum is 5 lines
+
+  // Colors for line, maximum is 5 lines
   const COLORS = ["#00A6CF", "#6A4C93", "#E76F51", "#2A9D8F", "#F4A261",
     "#264653", "#D62828"];
   let colorIndex = 0;
@@ -33,12 +37,12 @@
 
   function init() {
     chartSetUp();
-    id("generate-btn").addEventListener("click", e => {
-      e.preventDefault();
+    id("generate-btn").addEventListener("click", eventObject => {
+      eventObject.preventDefault();
       chartDataFetch();
     });
-    id("reset-btn").addEventListener("click", e => {
-      e.preventDefault();
+    id("reset-btn").addEventListener("click", eventObject => {
+      eventObject.preventDefault();
       resetBoard();
     });
     id("country-select").addEventListener("change", isOtherSelected);
@@ -58,8 +62,8 @@
     gdpChart.data.datasets = [];
     gdpChart.update();
 
-    let genBtn =  id("generate-btn");
-    if(genBtn.disabled) {
+    let genBtn = id("generate-btn");
+    if (genBtn.disabled) {
       id("generate-btn").disabled = false;
       qs("#info-form p").classList.add("hidden");
       colorIndex = 0;
@@ -73,62 +77,26 @@
     let popCanvas = id("population-chart");
     popChart = new Chart(popCanvas, {
       type: "line",
-      data: {
-        labels: years,
-        datasets: []
-      },
+      data: { labels: years, datasets: []},
       options: {
         scales: {
-          x: {
-            title: {
-              display: true,
-              text: "Year"
-            }
-          },
-          y: {
-            title: {
-              display: true,
-              text: "Population"
-            }
-          }
+          x: {title: {display: true, text: "Year"}},
+          y: {title: {display: true, text: "Population"}}
         },
-        plugins: {
-            title: {
-                display: true,
-                text: "Population chart"
-            }
-        }
+        plugins: {title: {display: true, text: "Population chart" }}
       }
     });
 
     let gdpCanvas = id("gdp-chart");
     gdpChart = new Chart(gdpCanvas, {
       type: "line",
-      data: {
-        labels: years,
-        datasets: []
-      },
+      data: {labels: years, datasets: []},
       options: {
         scales: {
-          x: {
-            title: {
-              display: true,
-              text: "Year"
-            }
-          },
-          y: {
-            title: {
-              display: true,
-              text: "GDP (current US$)"
-            }
-          }
+          x: {title: {display: true, text: "Year"}},
+          y: {title: {display: true, text: "GDP (current US$)"}}
         },
-        plugins: {
-            title: {
-                display: true,
-                text: "GDP chart"
-            }
-        }
+        plugins: {title: {display: true, text: "GDP chart" }}
       }
     });
   }
@@ -148,12 +116,11 @@
 
   function errorHandler(error) {
     id("error-page").classList.remove("hidden");
-    console.log(error);
   }
 
   function countryFetch() {
     let url = BASE_URL + "/country?format=json&per_page=400";
-    apiFetch(url, countrySelectFill)
+    apiFetch(url, countrySelectFill);
   }
   function countrySelectFill(countries) {
     let select = id("country-select");
@@ -166,7 +133,7 @@
 
     for (let i = 0; i < countries[0].total; i++) {
       let country = countries[1][i];
-      if(!existOption.has(country.iso2Code)) {
+      if (!existOption.has(country.iso2Code)) {
         let option = gen("option");
         option.textContent = country.name;
         option.value = country.iso2Code;
@@ -179,14 +146,16 @@
   function chartDataFetch() {
     let country = id("country-select").value;
 
-    let popUrl = BASE_URL
-      + "/country/" + country
-      + "/indicator/" + POPULATION_INDICATOR
-      + "?date=" + START_YEAR + ":" + END_YEAR + "&format=json";
-    let gdpUrl = BASE_URL
-      + "/country/" + country
-      + "/indicator/" + GDP_INDICATOR
-      + "?date=" + START_YEAR + ":" + END_YEAR + "&format=json";
+    let popUrl = BASE_URL +
+      "/country/" + country +
+      "/indicator/" + POPULATION_INDICATOR +
+      "?date=" + START_YEAR + ":" + END_YEAR +
+      "&format=json";
+    let gdpUrl = BASE_URL +
+      "/country/" + country +
+      "/indicator/" + GDP_INDICATOR +
+      "?date=" + START_YEAR + ":" + END_YEAR +
+      "&format=json";
 
     let color = (COLORS[colorIndex]);
     colorIndex++;
@@ -198,9 +167,9 @@
   function updateChart(chart, data, color) {
     let rows = data[1];
     const countryName = rows[0].country.value;
-    let values = rows.map(r => r.value).reverse();
+    let values = rows.map(row => row.value).reverse();
 
-    let lineExist = chart.data.datasets.find(d => d.label === countryName);
+    let lineExist = chart.data.datasets.find(dataset => dataset.label === countryName);
 
     if (!lineExist) {
       chart.data.datasets.push({
@@ -258,15 +227,6 @@
    */
   function qs(selector) {
     return document.querySelector(selector);
-  }
-
-  /**
-   * Returns the array of elements that match the given CSS selector.
-   * @param {string} query - CSS query selector
-   * @returns {object[]} array of DOM objects matching the query.
-   */
-  function qsa(query) {
-    return document.querySelectorAll(query);
   }
 
   /**
