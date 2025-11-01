9e65efd18df590df0d81b41abd4fef2d7c80f323
harrist <harrist@uw.edu>
2025-10-31 23:17:31 -0700

    Gogogo

--- index.js
+++ index.js
@@ -23,8 +23,8 @@
   // Indicator code for world bank data search
   const POPULATION_INDICATOR = "SP.POP.TOTL";
   const GDP_INDICATOR = "NY.GDP.MKTP.CD";
-  const AVERAGE_POPULATION_INDICATOR = "SP.POP.GROW";
-  const AVERAGE_GDP_INDICATOR = "NY.GDP.MKTP.KD.ZG";
+  const POPULATION_GROW_INDICATOR = "SP.POP.GROW";
+  const GDP_GROW_INDICATOR = "NY.GDP.MKTP.KD.ZG";
 
   window.addEventListener("load", init);
 
@@ -47,26 +47,30 @@
     }
   }
 
-  function apiFetch(url, handler) {
-    fetch(url)
+  function apiFetch(url) {
+    return fetch(url)
       .then(statusCheck)
       .then(res => res.json())
-      .then(handler)
-      .then(hideErrorPage)
-      .catch(errorHandler);
+      .then(data => {
+        hideErrorPage();
+        return data;
+      })
+      .catch(() => errorHandler("Opps! The country has no record. Unsecussful fetching"));
   }
 
   function hideErrorPage() {
     id("error-page").classList.add("hidden");
   }
 
-  function errorHandler() {
-    id("error-page").classList.remove("hidden");
+  function errorHandler(message) {
+    let errorPage = id("error-page");
+    errorPage.textContent = message;
+    errorPage.classList.remove("hidden");
   }
 
   function countrySelectFetch() {
     let url = BASE_URL + "/country?format=json&per_page=400";
-    apiFetch(url, countrySelectFill);
+    apiFetch(url).then(countrySelectFill)
   }
 
   function countrySelectFill(countries) {
@@ -93,36 +97,216 @@
   function countryDataFetch() {
     let country = id("country-select").value;
 
-    let countryUrl = BASE_URL + "/" + country + "?format=json";
-    let popUrl = BASE_URL +
-      "/country/" + country +
-      "/indicator/" + POPULATION_INDICATOR +
-      "?date=" + YEAR_END +
-      "&format=json";
-    let gdpUrl = BASE_URL +
-      "/country/" + country +
-      "/indicator/" + GDP_INDICATOR +
-      "?date=" + YEAR_END +
-      "&format=json";
-    let avgPopUrl = BASE_URL +
-      "/country/" + country +
-      "/indicator/" + AVERAGE_POPULATION_INDICATOR +
-      "?date=" + YEAR_START + ":" + YEAR_END +
-      "&format=json";
-    let avgGdpUrl = BASE_URL +
-      "/country/" + country +
-      "/indicator/" + AVERAGE_GDP_INDICATOR +
-      "?date=" + YEAR_START + ":" + YEAR_END +
-      "&format=json";
+    let cards = qsa(".country-card");
+    if(!isCardExist(cards, country)) {
+      let countryUrl = BASE_URL + "/country/" + country + "?format=json";
+      let popUrl = BASE_URL +
+        "/country/" + country +
+        "/indicator/" + POPULATION_INDICATOR +
+        "?date=" + YEAR_END +
+        "&format=json";
+      let gdpUrl = BASE_URL +
+        "/country/" + country +
+        "/indicator/" + GDP_INDICATOR +
+        "?date=" + YEAR_END +
+        "&format=json";
+      let popGrowUrl = BASE_URL +
+        "/country/" + country +
+        "/indicator/" + POPULATION_GROW_INDICATOR +
+        "?date=" + YEAR_START + ":" + YEAR_END +
+        "&format=json";
+      let agdpGrowUrl = BASE_URL +
+        "/country/" + country +
+        "/indicator/" + GDP_GROW_INDICATOR +
+        "?date=" + YEAR_START + ":" + YEAR_END +
+        "&format=json";
 
-      Promise.all()
-      makeCountryCard();
+      Promise.all([
+        apiFetch(countryUrl),
+        apiFetch(popUrl),
+        apiFetch(gdpUrl),
+        apiFetch(popGrowUrl),
+        apiFetch(agdpGrowUrl)
+      ])
+        .then(([countryData, popData, gdpData, avgPopData, avgGdpData]) => {
+          let card = gen("article");
+          card.classList.add("country-card");
+          card.id = countryData[1][0].iso2Code;
 
-    apiFetch(popUrl, data => updateChart(data, color));
-    apiFetch(gdpUrl, data => updateChart(data, color));
-    apiFetch(avgPopUrl, data => updateChart(data, color));
-    apiFetch(avgGdpUrl, data => updateChart(data, color));
-  }
+          headerSetup(card, countryData);
+          popRowSetup(card, popData);
+          gdpRowSetup(card, gdpData);
+          avgGdpRowSetup(card, popData, gdpData);
+          avgPopGrowthRowSetup(card, avgPopData);
+          avgGdpGrowthRowSetup(card, avgGdpData);
+
+          id("country-card-board").appendChild(card);
+        })
+        .then(hideErrorPage)
+        .catch(() => errorHandler("Something goes wrong in card generating!!"));
+      } else {
+        errorHandler("The card is already there, try another one!!");
+      }
+    }
+
+    function isCardExist(cards, country) {
+      for (let card of cards) {
+        if (card.id === country) {
+          return true;
+        }
+      }
+      return false;
+    }
+
+    function headerSetup(card, data) {
+      let country = data[1][0];
+      let name = country.name;
+      let income = country.incomeLevel.value ;
+
+      if (income === "High income") {
+        card.classList.add("high-income")
+      } else if (income === "Upper middle income") {
+        card.classList.add("upper-income");
+      } else if (income === "Lower middle income") {
+        card.classList.add("lower-income");
+      } else  {
+        card.classList.add("low-income");
+      }
+
+      let header = gen("header");
+      let title = gen("h3");
+      title.textContent = name;
+
+      let paragraph = gen("p");
+      paragraph.textContent = income;
+
+      header.appendChild(title);
+      header.appendChild(paragraph);
+      card.appendChild(header);
+    }
+
+    function popRowSetup(card, data) {
+      let pop = data[1][0].value;
+
+      let paragraph = gen("p");
+      paragraph.classList.add("card-row");
+
+      let label = gen("span");
+      label.classList.add("label");
+      label.textContent = "Population (" + YEAR_END + "):";
+
+      let value = gen("span");
+      value.classList.add("value");
+      value.textContent = pop;
+
+      paragraph.appendChild(label);
+      paragraph.appendChild(value);
+      card.appendChild(paragraph);
+    }
+
+    function gdpRowSetup(card, data) {
+      let gdp = Math.floor(data[1][0].value);
+
+      let paragraph = gen("p");
+      paragraph.classList.add("card-row");
+
+      let label = gen("span");
+      label.classList.add("label");
+      label.textContent = "GDP (" + YEAR_END + "):";
+
+      let value = gen("span");
+      value.classList.add("value");
+      value.textContent = "$" + gdp;
+
+      paragraph.appendChild(label);
+      paragraph.appendChild(value);
+      card.appendChild(paragraph);
+    }
+
+    function avgGdpRowSetup(card, popData, gdpData) {
+      let pop = popData[1][0].value;
+      let gdp = Math.floor(gdpData[1][0].value);
+      let avgGdp = Math.floor(gdp / pop);
+
+      let paragraph = gen("p");
+      paragraph.classList.add("card-row");
+
+      let label = gen("span");
+      label.classList.add("label");
+      label.textContent =  "GDP per capita";
+
+      let value = gen("span");
+      value.classList.add("value");
+      value.textContent = "$" + avgGdp;
+
+      paragraph.appendChild(label);
+      paragraph.appendChild(value);
+      card.appendChild(paragraph);
+    }
+
+    function avgPopGrowthRowSetup(card, data) {
+      let popGrow = data[1];
+      let avgPopGrowth = 0;
+      for(let i = 0; i < popGrow.length;  i++) {
+        avgPopGrowth += popGrow[i].value;
+      }
+      avgPopGrowth = Math.floor(avgPopGrowth / popGrow.length * 1000) / 1000;
+
+      let paragraph = gen("p");
+      paragraph.classList.add("card-row");
+      paragraph.classList.add("growth");
+
+      let label = gen("span");
+      label.classList.add("label");
+      label.textContent = "Average population growth";
+
+      let value = gen("span");
+      value.classList.add("value");
+      let growthLevel = checkGrowthLevel(avgPopGrowth);
+      value.classList.add(growthLevel);
+      value.textContent = avgPopGrowth + "%";
+
+      paragraph.appendChild(label);
+      paragraph.appendChild(value);
+      card.appendChild(paragraph);
+    }
+
+    function avgGdpGrowthRowSetup(card, data) {
+      let gdpGrow = data[1];
+      let avgGdpGrowth = 0;
+      for(let i = 0; i < gdpGrow.length;  i++) {
+        avgGdpGrowth += gdpGrow[i].value;
+      }
+      avgGdpGrowth = Math.floor(avgGdpGrowth / gdpGrow.length * 100) /100;
+
+      let paragraph = gen("p");
+      paragraph.classList.add("card-row");
+      paragraph.classList.add("growth");
+
+      let label = gen("span");
+      label.classList.add("label");
+      label.textContent = "Average gdp growth";
+
+      let value = gen("span");
+      value.classList.add("value");
+      let growthLevel = checkGrowthLevel(avgGdpGrowth);
+      value.classList.add(growthLevel);
+      value.textContent =  avgGdpGrowth + "%";
+
+      paragraph.appendChild(label);
+      paragraph.appendChild(value);
+      card.appendChild(paragraph);
+    }
+
+    function checkGrowthLevel(growth) {
+      if (growth >= 0.5) {
+        return "positive";
+      } else if (growth <= -0.5) {
+        return "negative";
+      } else {
+        return "neutral"
+      }
+    }
 
   /**
    * Helper function to return the response's result text if successful, otherwise
@@ -154,6 +338,15 @@
    */
   function qs(selector) {
     return document.querySelector(selector);
+  }
+
+  /**
+   * Returns the array of elements that match the given CSS selector.
+   * @param {string} query - CSS query selector
+   * @returns {object[]} array of DOM objects matching the query.
+   */
+  function qsa(query) {
+    return document.querySelectorAll(query);
   }
 
   /**
