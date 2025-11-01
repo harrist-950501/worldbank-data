547c49572918058de6fc3c5a6be17c3c489341a2
harrist <harrist@uw.edu>
2025-10-31 19:22:24 -0700

    chartJs test1

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  * Name: Harry Cheng
  * Date: 10/28/2025
  * Section: CSE 154 AA
- * This is the JS file for the index.html.
+ * This is the JS file that runs the data graphing tool.
  * It has the following features:
  *   sets up two line charts (Population and GDP) from the World Bank API
  *   allows users to choose a country to fetch data for
@@ -13,37 +13,29 @@
  */
 
 "use strict";
-(function (){
+(function() {
   const BASE_URL = "https://api.worldbank.org/v2";
 
   // Date for world bank data search
-  const START_YEAR = 2004;
-  const END_YEAR = 2023;
+  const YEAR_START = 2020;
+  const YEAR_END = 2023;
 
   // Indicator code for world bank data search
   const POPULATION_INDICATOR = "SP.POP.TOTL";
   const GDP_INDICATOR = "NY.GDP.MKTP.CD";
-
-  // The two chart
-  let popChart;
-  let gdpChart;
-
-  // Colors for line, maximum is 5 lines
-  const COLORS = ["#00A6CF", "#6A4C93", "#E76F51", "#2A9D8F", "#F4A261",
-    "#264653", "#D62828"];
-  let colorIndex = 0;
+  const AVERAGE_POPULATION_INDICATOR = "SP.POP.GROW";
+  const AVERAGE_GDP_INDICATOR = "NY.GDP.MKTP.KD.ZG";
 
   window.addEventListener("load", init);
 
   function init() {
-    chartSetUp();
-    id("generate-btn").addEventListener("click", eventObject => {
+    qs("#controller form").addEventListener("submit", eventObject => {
       eventObject.preventDefault();
-      chartDataFetch();
+      countryDataFetch();
     });
     id("reset-btn").addEventListener("click", eventObject => {
       eventObject.preventDefault();
-      resetBoard();
+      id("country-card-board").innerHTML = "";
     });
     id("country-select").addEventListener("change", isOtherSelected);
   }
@@ -51,54 +43,8 @@
   function isOtherSelected() {
     if (this.value === "other") {
       this.removeChild(this.lastElementChild);
-      countryFetch();
+      countrySelectFetch();
     }
-  }
-
-  function resetBoard() {
-    popChart.data.datasets = [];
-    popChart.update();
-
-    gdpChart.data.datasets = [];
-    gdpChart.update();
-
-    let genBtn = id("generate-btn");
-    if (genBtn.disabled) {
-      id("generate-btn").disabled = false;
-      qs("#info-form p").classList.add("hidden");
-      colorIndex = 0;
-    }
-
-  }
-
-  function chartSetUp() {
-    let years = Array.from({ length: END_YEAR - START_YEAR + 1}, (_, i) => START_YEAR + i);
-
-    let popCanvas = id("population-chart");
-    popChart = new Chart(popCanvas, {
-      type: "line",
-      data: { labels: years, datasets: []},
-      options: {
-        scales: {
-          x: {title: {display: true, text: "Year"}},
-          y: {title: {display: true, text: "Population"}}
-        },
-        plugins: {title: {display: true, text: "Population chart" }}
-      }
-    });
-
-    let gdpCanvas = id("gdp-chart");
-    gdpChart = new Chart(gdpCanvas, {
-      type: "line",
-      data: {labels: years, datasets: []},
-      options: {
-        scales: {
-          x: {title: {display: true, text: "Year"}},
-          y: {title: {display: true, text: "GDP (current US$)"}}
-        },
-        plugins: {title: {display: true, text: "GDP chart" }}
-      }
-    });
   }
 
   function apiFetch(url, handler) {
@@ -114,14 +60,15 @@
     id("error-page").classList.add("hidden");
   }
 
-  function errorHandler(error) {
+  function errorHandler() {
     id("error-page").classList.remove("hidden");
   }
 
-  function countryFetch() {
+  function countrySelectFetch() {
     let url = BASE_URL + "/country?format=json&per_page=400";
     apiFetch(url, countrySelectFill);
   }
+
   function countrySelectFill(countries) {
     let select = id("country-select");
     let existOption = new Set();
@@ -143,58 +90,38 @@
 
   }
 
-  function chartDataFetch() {
+  function countryDataFetch() {
     let country = id("country-select").value;
 
+    let countryUrl = BASE_URL + "/" + country + "?format=json";
     let popUrl = BASE_URL +
       "/country/" + country +
       "/indicator/" + POPULATION_INDICATOR +
-      "?date=" + START_YEAR + ":" + END_YEAR +
+      "?date=" + YEAR_END +
       "&format=json";
     let gdpUrl = BASE_URL +
       "/country/" + country +
       "/indicator/" + GDP_INDICATOR +
-      "?date=" + START_YEAR + ":" + END_YEAR +
+      "?date=" + YEAR_END +
+      "&format=json";
+    let avgPopUrl = BASE_URL +
+      "/country/" + country +
+      "/indicator/" + AVERAGE_POPULATION_INDICATOR +
+      "?date=" + YEAR_START + ":" + YEAR_END +
+      "&format=json";
+    let avgGdpUrl = BASE_URL +
+      "/country/" + country +
+      "/indicator/" + AVERAGE_GDP_INDICATOR +
+      "?date=" + YEAR_START + ":" + YEAR_END +
       "&format=json";
 
-    let color = (COLORS[colorIndex]);
-    colorIndex++;
+      Promise.all()
+      makeCountryCard();
 
-    apiFetch(popUrl, data => updateChart(popChart, data, color));
-    apiFetch(gdpUrl, data => updateChart(gdpChart, data, color));
-  }
-
-  function updateChart(chart, data, color) {
-    let rows = data[1];
-    const countryName = rows[0].country.value;
-    let values = rows.map(row => row.value).reverse();
-
-    let lineExist = chart.data.datasets.find(dataset => dataset.label === countryName);
-
-    if (!lineExist) {
-      chart.data.datasets.push({
-        label: countryName,
-        data: values,
-        borderColor: color,
-        backgroundColor: color + "33", // 半透明背景
-        borderWidth: 2,
-        tension: 0.3,
-        pointRadius: 3,
-        pointHoverRadius: 5,
-        pointHoverBackgroundColor: "#FFFFFF"
-      });
-    }
-
-    chart.update();
-
-    isChartLineOver(chart.data.datasets.length);
-  }
-
-  function isChartLineOver(length) {
-    if (length === COLORS.length) {
-      id("generate-btn").disabled = true;
-      qs("#info-form p").classList.remove("hidden");
-    }
+    apiFetch(popUrl, data => updateChart(data, color));
+    apiFetch(gdpUrl, data => updateChart(data, color));
+    apiFetch(avgPopUrl, data => updateChart(data, color));
+    apiFetch(avgGdpUrl, data => updateChart(data, color));
   }
 
   /**
